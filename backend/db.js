051b00fef0eb157ddbddd33fae5aead629bf3740
Anushka Jana <your-email@example.com>
2026-05-08 02:56:59 +0530

    fix db connection

--- backend/db.js
+++ backend/db.js
@@ -5,13 +5,17 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 db.connect((err) => {
   if (err) {
     console.error("DB connection failed:", err);
   } else {
-    console.log("MySQL Connected");
+    console.log("Connected to Railway MySQL");
   }
 });
 
