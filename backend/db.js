c06d64de1cfbb28d2bc82e9c4b30d0691816982d
Anushka Jana <your-email@example.com>
2026-05-08 19:47:20 +0530

    fix: remove env and use render env

--- backend/db.js
+++ backend/db.js
@@ -5,7 +5,7 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT),
   ssl: {
     rejectUnauthorized: false,
   },
@@ -13,9 +13,9 @@ const db = mysql.createConnection({
 
 db.connect((err) => {
   if (err) {
-    console.error("DB connection failed:", err);
+    console.error("DB ERROR:", err);
   } else {
-    console.log("Connected to Railway MySQL");
+    console.log("Connected to Railway MySQL ✅");
   }
 });
 
